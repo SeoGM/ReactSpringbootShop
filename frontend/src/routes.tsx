d605ea8e3683190c6456e85561aa514ac99ec6e7
서광명 <auddlzz000@gmail.com>
2024-08-21 14:54:32 +0000

    layout과 components 분리하여 관리

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ShopLayout from './components/ShopLayout';
-import AdminLayout from './components/AdminLayout';
+import ShopLayout from './layout/ShopLayout';
+import AdminLayout from './layout/AdminLayout';
 import MainPage from './features/main/pages/MainPage';
 import LoginPage from './features/user/pages/LoginPage';
 import RegisterPage from './features/user/pages/RegisterPage';
