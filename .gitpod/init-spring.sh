ef4d5f65f53ae57297a70227d92e85ba89fdd24c
GwangMyeong Seo <auddlzz000@gmail.com>
2024-08-30 08:24:16 +0000

    gitpod.yml 백엔드 설정중

---
+++ .gitpod/init-spring.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd backend
+
+# Install SDKMAN and set up Java 17
+curl -s "https://get.sdkman.io" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+yes | sdk install java 17.0.4-tem
+
+./gradlew build -x test
+
+./gradlew bootRun
